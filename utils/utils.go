f2ae9ade4d311af4696c3af1cb87ead3a5526f1a
zhongxuan_li <aa234258202@qq.com>
2024-07-05 14:52:32 +0800

    add checker

--- utils/utils.go
+++ utils/utils.go
@@ -2,44 +2,10 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/urfave/cli"
 	"io"
 	"os"
 	"path/filepath"
 )
-
-const (
-	exactArgs = iota
-	minArgs
-	maxArgs
-)
-
-func checkArgs(context *cli.Context, expected, checkType int) error {
-	var err error
-	cmdName := context.Command.Name
-	switch checkType {
-	case exactArgs:
-		if context.NArg() != expected {
-			err = fmt.Errorf("%s: %q requires exactly %d argument(s)", os.Args[0], cmdName, expected)
-		}
-	case minArgs:
-		if context.NArg() < expected {
-			err = fmt.Errorf("%s: %q requires a minimum of %d argument(s)", os.Args[0], cmdName, expected)
-		}
-	case maxArgs:
-		if context.NArg() > expected {
-			err = fmt.Errorf("%s: %q requires a maximum of %d argument(s)", os.Args[0], cmdName, expected)
-		}
-	}
-
-	if err != nil {
-		fmt.Printf("Incorrect Usage.\n\n")
-		_ = cli.ShowCommandHelp(context, cmdName)
-		return err
-	}
-	return nil
-}
 
 func WriteJSON(w io.Writer, v interface{}) error {
 	data, err := json.Marshal(v)
